6924bf46fa2ddf7307124b29aace3189faaabdd4
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2022-11-13 14:36:11 -0600

    Update formChecker.js

--- formChecker.js
+++ formChecker.js
@@ -1,6 +1,12 @@
 function validateForm()
 {
-    let path = document.getElementById("fuzzysocks");
-    alert(path.value)
+    let chessset = document.getElementById("chessset");
+    if(chessset.value == "" || chessset.value < 0)
+    {
+        alert("Invalid quantity selected for Chess Sets!");
+    }
+    let airfryer = document.getElementById("airfryer");
+    let grandpiano = document.getElementById("grandpiano");
+    let fuzzysocks = document.getElementById("fuzzysocks");
     return false;
 }
