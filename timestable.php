e3169123dcc4f5a3d9ab88350a9c786b42cf5f59
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2022-11-13 10:19:29 -0600

    Step 1 complete

--- timestable.php
+++ timestable.php
@@ -8,7 +8,7 @@ for($i = 0; $i <= 100; $i++){
 	echo "<tr>";
 	if($i == 0){
 		echo "<td>*</td>";
-		for($j = 1; $j < 100; $j++){
+		for($j = 1; $j <= 100; $j++){
 			echo "<td>" . $j . "</td>";
 		}
 	}
