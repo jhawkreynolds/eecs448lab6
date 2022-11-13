cf4db0ef3e90db3ece8a3d99e7344704df65b2bc
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2022-11-13 12:42:30 -0600

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,3 +1,12 @@
 <?php
-   echo "<p>Hello, World!</p>";
+    echo "<h2>Question 1:</h2>";
+    echo "<p>Which of these buildings is NOT on Jayhawk Boulevard?</p>";
+    echo "<h2>Question 2:</h2>";
+    echo "<p>If you enter from the front entrance, which floor of the Kansas Union are you on?</p>";
+    echo "<h2>Question 3:</h2>";
+    echo "<p>What is the westernmost street on the east side of campus that connects to both 6th and 23rd Street?</p>";
+    echo "<h2>Question 4:</h2>";
+    echo "<p>What two buildings on campus can you NOT get between without going outside?</p>";
+    echo "<h2>Question 5:</h2>";
+    echo "<p>The tunnel by the School of Engineering helps you avoid crossing which street?</p>";
 ?>
