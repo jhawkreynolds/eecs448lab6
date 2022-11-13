290382bd68bac5c4e6c2e48879080cb033cd0377
jhawkreynolds <112728229+jhawkreynolds@users.noreply.github.com>
2022-11-13 09:37:33 -0600

    Added timestable.php

---
+++ timestable.php
@@ -0,0 +1,24 @@
+<?php
+function product($x, $y){
+	$z = $x * $y;
+	return $z;
+}
+echo "<table>";
+for($i = 0; $i <= 100; $i++){
+	echo "<tr>";
+	if($i == 0){
+		echo "<td>*</td>";
+		for($j = 1; $j < 100; $j++){
+			echo "<td>" . $j . "</td>";
+		}
+	}
+	if($i > 0){
+		echo "<td>" . $i . " </td>";
+		for($j = 1; $j < 100; $j++){
+			echo "<td>" . product($i,$j) . "</td>";
+		}
+	}
+	echo "</tr>";
+}
+echo "</table>";
+?>
